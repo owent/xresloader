34d461b91830757e051465bd1275bd1a1eee9c9f
owent <owt5008137@live.com>
2016-12-11 01:22:09 +0800

    没有 -m 参数时不会崩

--- src/com/owent/xresloader/scheme/SchemeDataSourceCmd.java
+++ src/com/owent/xresloader/scheme/SchemeDataSourceCmd.java
@@ -11,7 +11,7 @@ public class SchemeDataSourceCmd extends SchemeDataSourceBase {
     @Override
     public int load() {
         // 至少有四个必须字段，所以少于四项的肯定错
-        if(ProgramOptions.getInstance().dataSourceMetas.length < 4) {
+        if(null == ProgramOptions.getInstance().dataSourceMetas || ProgramOptions.getInstance().dataSourceMetas.length < 4) {
             ProgramOptions.getLoger().error("Data source error. DataSource,ProtoName,OutputFile and KeyRow is required.");
             return -21;
         }
@@ -22,10 +22,15 @@ public class SchemeDataSourceCmd extends SchemeDataSourceBase {
     @Override
     public boolean load_scheme(String table) {
         for(String cfg_val : ProgramOptions.getInstance().dataSourceMetas) {
-            int eq_index = cfg_val.indexOf('=');
+            String unwrap_val = cfg_val;
+            if (unwrap_val.length() > 2 && ('"' == unwrap_val.charAt(0) || '\'' == unwrap_val.charAt(0)) && unwrap_val.charAt(0) == unwrap_val.charAt(unwrap_val.length() - 1)) {
+                unwrap_val = unwrap_val.substring(1, unwrap_val.length() - 1);
+            }
+
+            int eq_index = unwrap_val.indexOf('=');
             if (eq_index < 0) {
                 ProgramOptions.getLoger().error("Data meta %s error.Must be KEY=%s",
-                        cfg_val,
+                        unwrap_val,
                         String.join(
                             ProgramOptions.getInstance().dataSourceMetaDelimiter.replaceAll("\\(.)", "$1"),
                             "VALUE1", "VALUE2", "VALUE3"
@@ -36,8 +41,8 @@ public class SchemeDataSourceCmd extends SchemeDataSourceBase {
 
             ArrayList<String> vals = new ArrayList<String>();
             vals.ensureCapacity(3);
-            if (cfg_val.length() > eq_index + 1) {
-                for(String vn: cfg_val.substring(eq_index + 1).split(ProgramOptions.getInstance().dataSourceMetaDelimiter)) {
+            if (unwrap_val.length() > eq_index + 1) {
+                for(String vn: unwrap_val.substring(eq_index + 1).split(ProgramOptions.getInstance().dataSourceMetaDelimiter)) {
                     vals.add(vn.trim());
                 }
             }
@@ -46,7 +51,7 @@ public class SchemeDataSourceCmd extends SchemeDataSourceBase {
                 vals.add("");
             }
 
-            set_scheme(cfg_val.substring(0, eq_index).trim(), vals);
+            set_scheme(unwrap_val.substring(0, eq_index).trim(), vals);
         }
 
         boolean ret = true;
